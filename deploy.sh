2401cb98ff9578e6701a3aac3b1be7cd75f1680b
Russ Moul <rgmoul@comcast.net>
2023-01-05 06:51:47 -0800

    Initial upload

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #! /bin/zsh
 
- mvn deploy:deploy-file -Durl=file:/Users/russ/udub/uwpce/repository -DrepositoryId=uwpce -Dfile=/Users/russ/udub/cp520/cp520-stubs/target/cp520-stubs-1.0.jar -DpomFile=/Users/russ/udub/cp520/cp520-stubs/pom.xml
- mvn deploy:deploy-file -Durl=file:/Users/russ/udub/uwpce/repository -DrepositoryId=uwpce -Dfile=/Users/russ/udub/cp520/cp520-support/target/cp520-support-1.0.jar -DpomFile=/Users/russ/udub/cp520/cp520-support/pom.xml
- mvn deploy:deploy-file -Durl=file:/Users/russ/udub/uwpce/repository -DrepositoryId=uwpce -Dfile=/Users/russ/udub/cp520/cp520-support-deps.pom -DpomFile=/Users/russ/udub/cp520/cp520-support-deps.pom -Dpackaging=pom
- mvn deploy:deploy-file -Durl=file:/Users/russ/udub/uwpce/repository -DrepositoryId=uwpce -Dfile=/Users/russ/udub/cp520/cp520-support-deps.pom -DpomFile=/Users/russ/udub/cp520/cp520-support-deps.pom -Dpackaging=pom
+ mvn deploy:deploy-file -Durl=file:repository -DrepositoryId=uwpce -Dfile=cp520-deps.pom -DpomFile=cp520-deps.pom -Dpackaging=pom
+ mvn deploy:deploy-file -Durl=file:repository -DrepositoryId=uwpce -Dfile=cp520-support-deps.pom -DpomFile=cp520-support-deps.pom -Dpackaging=pom
+ mvn deploy:deploy-file -Durl=file:repository -DrepositoryId=uwpce -Dfile=../cp520/cp520-stubs/target/cp520-stubs-1.0.jar -DpomFile=../cp520/cp520-stubs/pom.xml -Dpackaging=jar
+ mvn deploy:deploy-file -Durl=file:repository -DrepositoryId=uwpce -Dfile=../cp520/cp520-support/target/cp520-support-1.0.jar -DpomFile=../cp520/cp520-support/pom.xml -Dpackaging=jar
+ 
 
